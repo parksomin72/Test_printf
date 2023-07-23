467fb5fab10ae969905edb0141c089251dd99e9f
parksomin72 <fatimazahraealawi@gmail.com>
2023-07-23 13:13:49 -0700

    new

--- write_handlers.c
+++ write_handlers.c
@@ -60,8 +60,36 @@ if (extra_c != 0)
 length++;
 if (width > length)
 {
-for (i=1; i < width - length + 1; i++)
+for (i = 1; i < width - length + 1; i++)
 buffer[i] = padd;
 buffer[i] = '\0';
 if (flags & F_MINUS && padd == ' ')
 /*Asign extra char to left of buffer*/
+{
+if (extra_c)
+buffer[--ind] = extra_c;
+return (write(1, &buffer[ind], length) + write(1, &buffer[1], i - 1));
+}
+else if (!(flags & F_MINUS) && padd == ' ')
+/** extar char to left of buff **/
+{
+if (extra_c)
+buffer[--ind] = extra_c;
+return (write(1, &buffer[1], i - 1) + write(1, &buffer[ind], length));
+}
+else if (!(flags & F_MINUS) && padd == '0')
+/** extra char to left of padd **/
+{
+if (extra_c)
+buffer[--padd_start] = extra_c;
+return (write(1, &buffer[padd_start], i - padd_start) +
+write(1, &buffer[ind], length - (1 - padd_start)));
+}
+}
+if (extra_c)
+buffer[--ind] = extra_c;
+return (write(1, &buffer[ind], length));
+}
+/**
+ *
+ */
