4a08ad532d6087e0bbf4be1c4e2903fef57dd54c
parksomin72 <fatimazahraealawi@gmail.com>
2023-07-23 17:45:47 -0700

    new

--- write_handlers.c
+++ write_handlers.c
@@ -150,10 +150,81 @@ return (write(1, &buffer[ind], length));
  *
  * Return: Number of written chars
  */
-int write_pointer(char buffer[], int ind, int length, int width,
-int flags, char padd, char padd, char extra_c, int padd_start)
+int write_pointer(char buffer[], char padd, char extra_c, int ind,
+int length, int width, int flags, int padd_start)
 {
 int i;
 if (width > length)
 {
 for (i = 3; i < width - length + 3; i++)
+buffer[i] = padd;
+buffer[i] = '\0';
+if (flags & F_MINUS && padd == ' ')
+{
+buffer[--ind] = 'x';
+buffer[--ind] = '0';
+if (extra_c)
+buffer[--ind] = extra_c;
+return (write(1, &buffer[ind], length) + write(1, &buffer[3], i - 3));
+}
+else if (!(flags & F_MINUS) && padd == ' ')
+{
+buffer[--ind] = 'x';
+buffer[--ind] = '0';
+if (extra_c)
+buffer[--ind] = extra_c;
+return (write(1, &buffer[3], i - 3) + write(1, &buffer[ind], length));
+}
+else if (!(flags & F_MINUS) && padd == '0')
+{
+if (extra_c)
+buffer[--padd_start] = extra_c;
+buffer[1] = '0';
+buffer[2] = 'x';
+return (write(1, &buffer[padd_start], i - padd_start) +
+write(1, &buffer[ind], length - (1 - padd_start) - 2));
+}
+}
+buffer[--ind] = 'x';
+buffer[--ind] = '0';
+if (extra_c)
+buffer[--ind] = extra_c;
+return (write(1, &buffer[ind], BUFF_SIZE - ind - 1));
+}
+/******************** WRITE HANDLE ************************/
+/**
+ * handle_write_char - Prints a string
+ * @c: Char types
+ * @buffer: Buffer array to handle print
+ * @flags: Calculates active flags
+ * @width: get Width
+ * @precision: Precision specifier
+ * @size: Size specifier
+ *
+ * Return: Number of chrs printed
+ */
+int handle_write_char(char c, char buffer[], int flags, int width,
+int precision, int size)
+{
+int i = 0;
+char padd = ' ';
+UNUSED(precision);
+UNUSED(size);
+if (flags & F_ZERO)
+padd = '0';
+buffer[i++] = c;
+buffer[i] = '\0';
+if (width > 1)
+{
+buffer[BUFF_SIZE - 1] = '\0';
+for (i = 0; i < width - 1; i++)
+buffer[BUFF_SIZE - i - 2] = padd;
+if (flags & F_MINUS)
+return (write(1, &buffer[0], 1) +
+write(1, &buffer[BUFF_SIZE - i - 1], width - 1));
+else
+return (write(1, &buffer[BUFF_SIZE - i - 1], width - 1) +
+write(1, &buffer[0], 1));
+}
+return (write(1, &buffer[0], 1));
+}
